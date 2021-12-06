9e531a94d97dd41efaef7ccb641b472b130e878e
pop4959 <pop4959@gmail.com>
2021-12-05 22:27:57 -0800

    Check if plugins are enabled before enabling integration

--- bukkit/src/main/java/org/popcraft/chunkyborder/BukkitMapIntegrationLoader.java
+++ bukkit/src/main/java/org/popcraft/chunkyborder/BukkitMapIntegrationLoader.java
@@ -2,6 +2,7 @@ package org.popcraft.chunkyborder;
 
 import net.pl3x.map.api.Pl3xMap;
 import net.pl3x.map.api.Pl3xMapProvider;
+import org.bukkit.plugin.PluginManager;
 import org.dynmap.DynmapAPI;
 import org.popcraft.chunky.integration.BlueMapIntegration;
 import org.popcraft.chunky.integration.DynmapIntegration;
@@ -11,6 +12,9 @@ import org.popcraft.chunkyborder.platform.MapIntegrationLoader;
 import java.util.Optional;
 
 public class BukkitMapIntegrationLoader implements MapIntegrationLoader {
+    private static final String BLUEMAP = "BlueMap";
+    private static final String DYNMAP = "dynmap";
+    private static final String PL3XMAP = "Pl3xMap";
     private final ChunkyBorderBukkit chunkyBorderBukkit;
 
     public BukkitMapIntegrationLoader(ChunkyBorderBukkit chunkyBorderBukkit) {
@@ -19,13 +23,21 @@ public class BukkitMapIntegrationLoader implements MapIntegrationLoader {
 
     @Override
     public Optional<BlueMapIntegration> loadBlueMap() {
-        return Optional.ofNullable(chunkyBorderBukkit.getServer().getPluginManager().getPlugin("BlueMap"))
+        final PluginManager pluginManager = chunkyBorderBukkit.getServer().getPluginManager();
+        if (!pluginManager.isPluginEnabled(BLUEMAP)) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(chunkyBorderBukkit.getServer().getPluginManager().getPlugin(BLUEMAP))
                 .map(blueMap -> new BlueMapIntegration());
     }
 
     @Override
     public Optional<DynmapIntegration> loadDynmap() {
-        return Optional.ofNullable(chunkyBorderBukkit.getServer().getPluginManager().getPlugin("dynmap"))
+        final PluginManager pluginManager = chunkyBorderBukkit.getServer().getPluginManager();
+        if (!pluginManager.isPluginEnabled(DYNMAP)) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(chunkyBorderBukkit.getServer().getPluginManager().getPlugin(DYNMAP))
                 .map(dynmap -> {
                     DynmapAPI dynmapAPI = (DynmapAPI) dynmap;
                     return dynmapAPI.markerAPIInitialized() ? new DynmapIntegration(dynmapAPI) : null;
@@ -34,7 +46,11 @@ public class BukkitMapIntegrationLoader implements MapIntegrationLoader {
 
     @Override
     public Optional<Pl3xMapIntegration> loadPl3xMap() {
-        return Optional.ofNullable(chunkyBorderBukkit.getServer().getPluginManager().getPlugin("Pl3xMap"))
+        final PluginManager pluginManager = chunkyBorderBukkit.getServer().getPluginManager();
+        if (!pluginManager.isPluginEnabled(PL3XMAP)) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(pluginManager.getPlugin(PL3XMAP))
                 .map(pl3xMap -> {
                     try {
                         Pl3xMap pl3xMapAPI = Pl3xMapProvider.get();
