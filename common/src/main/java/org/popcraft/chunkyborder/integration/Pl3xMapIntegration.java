eceae52e37cdbeca140756be7a71fc0da1f86757
BillyGalbreath <blake.galbreath@gmail.com>
2022-09-17 02:10:29 -0500

    fix issues with per-world layers

--- common/src/main/java/org/popcraft/chunkyborder/integration/Pl3xMapIntegration.java
+++ common/src/main/java/org/popcraft/chunkyborder/integration/Pl3xMapIntegration.java
@@ -10,10 +10,7 @@ import net.pl3x.map.markers.layer.Layer;
 import net.pl3x.map.markers.layer.SimpleLayer;
 import net.pl3x.map.markers.layer.WorldBorderLayer;
 import net.pl3x.map.markers.marker.Marker;
-import net.pl3x.map.markers.option.Fill;
 import net.pl3x.map.markers.option.Options;
-import net.pl3x.map.markers.option.Stroke;
-import net.pl3x.map.markers.option.Tooltip;
 import net.pl3x.map.util.Colors;
 import net.pl3x.map.world.World;
 import org.popcraft.chunky.platform.util.Vector2;
@@ -25,13 +22,16 @@ import org.popcraft.chunkyborder.BorderData;
 import org.popcraft.chunkyborder.ChunkyBorder;
 import org.popcraft.chunkyborder.ChunkyBorderProvider;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class Pl3xMapIntegration extends AbstractMapIntegration {
     private static final Key CHUNKY_KEY = Key.of("chunky");
     private final Pl3xMap pl3xMap;
-    private Layer defaultLayer;
-    private Layer chunkyLayer;
+    private final Map<World, Layer> defaultLayers = new HashMap<>();
+    private boolean hideByDefault;
+    private int priority;
     private Options markerOptions;
 
     public Pl3xMapIntegration(final Pl3xMap pl3xMap) {
@@ -39,7 +39,7 @@ public class Pl3xMapIntegration extends AbstractMapIntegration {
         pl3xMap.getEventRegistry().register(new EventListener() {
             @EventHandler
             @SuppressWarnings("unused")
-            public void onWorldLoaded(WorldLoadedEvent event) {
+            public void onWorldLoaded(final WorldLoadedEvent event) {
                 final ChunkyBorder chunkyBorder = ChunkyBorderProvider.get();
                 chunkyBorder.getChunky().getServer().getWorld(event.getWorld().getName())
                         .ifPresent(world -> chunkyBorder.getBorder(world.getName())
@@ -52,7 +52,7 @@ public class Pl3xMapIntegration extends AbstractMapIntegration {
     @Override
     public void addShapeMarker(final org.popcraft.chunky.platform.World world, final Shape shape) {
         getWorld(world).ifPresent(pl3xmapWorld -> {
-            final Marker marker;
+            final Marker<?> marker;
             if (shape instanceof final AbstractPolygon polygon) {
                 marker = Marker.polyline(polygon.points().stream()
                         .map(point -> Point.of(point.getX(), point.getZ()))
@@ -79,35 +79,46 @@ public class Pl3xMapIntegration extends AbstractMapIntegration {
 
     @Override
     public void removeAllShapeMarkers() {
-        pl3xMap.getWorldRegistry().entries().values().forEach(this::resetLayer);
+        this.pl3xMap.getWorldRegistry().entries().values().forEach(this::resetLayer);
     }
 
     @Override
     public void setOptions(final String label, final String color, final boolean hideByDefault, final int priority, final int weight) {
         super.setOptions(label, color, hideByDefault, priority, weight);
-        this.markerOptions = new Options(new Stroke(this.weight, Colors.setAlpha(0xFF, this.color)), new Fill(false), new Tooltip(this.label), null);
-        this.chunkyLayer = new SimpleLayer(CHUNKY_KEY, () -> this.label)
-                .setDefaultHidden(hideByDefault)
-                .setPriority(1).setZIndex(priority);
+        this.hideByDefault = hideByDefault;
+        this.priority = priority;
+        this.markerOptions = Options.builder()
+                .strokeWeight(this.weight)
+                .strokeColor(Colors.setAlpha(0xFF, this.color))
+                .fill(false)
+                .tooltipContent(this.label)
+                .tooltipSticky(true)
+                .build();
     }
 
     private SimpleLayer getLayer(final World world) {
-        final Layer worldBorderLayer = world.getLayerRegistry().get(WorldBorderLayer.KEY);
-        if (defaultLayer != null) {
-            this.defaultLayer = worldBorderLayer;
-            world.getLayerRegistry().unregister(WorldBorderLayer.KEY);
+        final Layer worldBorderLayer = world.getLayerRegistry().unregister(WorldBorderLayer.KEY);
+        if (!this.defaultLayers.containsKey(world)) {
+            this.defaultLayers.put(world, worldBorderLayer);
         }
-        return (SimpleLayer) world.getLayerRegistry().getOrRegister(CHUNKY_KEY, this.chunkyLayer);
+        Layer chunkyLayer = world.getLayerRegistry().get(CHUNKY_KEY);
+        if (chunkyLayer == null) {
+            chunkyLayer = new SimpleLayer(CHUNKY_KEY, () -> this.label)
+                    .setDefaultHidden(this.hideByDefault)
+                    .setPriority(1)
+                    .setZIndex(this.priority);
+            world.getLayerRegistry().register(chunkyLayer);
+        }
+        return (SimpleLayer) chunkyLayer;
     }
 
     private void resetLayer(final World world) {
         world.getLayerRegistry().unregister(CHUNKY_KEY);
-        if (defaultLayer != null) {
-            world.getLayerRegistry().register(defaultLayer);
-        }
+        world.getLayerRegistry().register(this.defaultLayers.get(world));
+        this.defaultLayers.remove(world);
     }
 
-    private Optional<World> getWorld(org.popcraft.chunky.platform.World world) {
-        return Optional.ofNullable(pl3xMap.getWorldRegistry().get(world.getName())).filter(World::isEnabled);
+    private Optional<World> getWorld(final org.popcraft.chunky.platform.World world) {
+        return Optional.ofNullable(this.pl3xMap.getWorldRegistry().get(world.getName())).filter(World::isEnabled);
     }
 }
