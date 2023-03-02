b84d3db25655c976c0fe4b744ef5c893112d7099
Christopher Bohn <pop4959@gmail.com>
2023-03-01 20:44:13 -0800

    Shortcut arguments for border add

--- common/src/main/java/org/popcraft/chunkyborder/command/AddCommand.java
+++ common/src/main/java/org/popcraft/chunkyborder/command/AddCommand.java
@@ -7,6 +7,7 @@ import org.popcraft.chunky.command.CommandArguments;
 import org.popcraft.chunky.platform.Sender;
 import org.popcraft.chunky.platform.World;
 import org.popcraft.chunky.shape.ShapeFactory;
+import org.popcraft.chunky.shape.ShapeType;
 import org.popcraft.chunky.util.Formatting;
 import org.popcraft.chunky.util.Input;
 import org.popcraft.chunky.util.TranslationKey;
@@ -28,8 +29,8 @@ public class AddCommand implements ChunkyCommand {
 
     @Override
     public void execute(final Sender sender, final CommandArguments arguments) {
+        final Chunky chunky = chunkyBorder.getChunky();
         if (arguments.size() > 1) {
-            final Chunky chunky = chunkyBorder.getChunky();
             final Optional<World> world = arguments.next().flatMap(arg -> Input.tryWorld(chunky, arg));
             if (world.isPresent()) {
                 chunky.getSelection().world(world.get());
@@ -38,7 +39,44 @@ public class AddCommand implements ChunkyCommand {
                 return;
             }
         }
-        final Selection selection = chunkyBorder.getChunky().getSelection().build();
+        if (arguments.size() > 2) {
+            final Optional<String> shape = arguments.next().flatMap(Input::tryShape);
+            if (shape.isPresent()) {
+                chunky.getSelection().shape(shape.get());
+            } else {
+                sender.sendMessage(TranslationKey.HELP_BORDER);
+                return;
+            }
+        }
+        if (arguments.size() > 3) {
+            final Optional<Double> centerX = arguments.next().flatMap(Input::tryDoubleSuffixed).filter(c -> !Input.isPastWorldLimit(c));
+            final Optional<Double> centerZ = arguments.next().flatMap(Input::tryDoubleSuffixed).filter(c -> !Input.isPastWorldLimit(c));
+            if (centerX.isPresent() && centerZ.isPresent()) {
+                chunky.getSelection().center(centerX.get(), centerZ.get());
+            } else {
+                sender.sendMessage(TranslationKey.HELP_BORDER);
+                return;
+            }
+        }
+        if (arguments.size() > 5) {
+            final Optional<Double> radiusX = arguments.next().flatMap(Input::tryDoubleSuffixed).filter(r -> r >= 0 && !Input.isPastWorldLimit(r));
+            if (radiusX.isPresent()) {
+                chunky.getSelection().radius(radiusX.get());
+            } else {
+                sender.sendMessage(TranslationKey.HELP_BORDER);
+                return;
+            }
+        }
+        if (arguments.size() > 6) {
+            final Optional<Double> radiusZ = arguments.next().flatMap(Input::tryDoubleSuffixed).filter(r -> r >= 0 && !Input.isPastWorldLimit(r));
+            if (radiusZ.isPresent()) {
+                chunky.getSelection().radiusZ(radiusZ.get());
+            } else {
+                sender.sendMessage(TranslationKey.HELP_BORDER);
+                return;
+            }
+        }
+        final Selection selection = chunky.getSelection().build();
         final World world = selection.world();
         final BorderData borderData = new BorderData(selection);
         final Map<String, BorderData> borders = chunkyBorder.getBorders();
@@ -65,6 +103,8 @@ public class AddCommand implements ChunkyCommand {
             final List<String> suggestions = new ArrayList<>();
             chunkyBorder.getChunky().getServer().getWorlds().forEach(world -> suggestions.add(world.getName()));
             return suggestions;
+        } else if (commandArguments.size() == 3) {
+            return ShapeType.all();
         }
         return List.of();
     }
